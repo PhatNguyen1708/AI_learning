a3b7ba1a52fa845672f1f2f9f9bbb0acfa810e82
PhatNguyen1708 <144696148+PhatNguyen1708@users.noreply.github.com>
2024-09-25 11:00:13 +0700

    .

--- test.py
+++ test.py
@@ -2,16 +2,15 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox, scrolledtext
 from PIL import Image,ImageTk
-from tkinter import filedialog 
 import networkx as nx
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import re
 #colors
-co0 = "#ffffff" 
-co1 = "#000000"  
-co2 = "#4456F0"  
-co4 = "#6495ED"   
+co0 = "#ffffff" #trang
+co1 = "#000000"  #den
+co2 = "#4456F0"  #xanh
+co4 = "#6495ED"   #hongnhat
 co5 = "#dda0dd"
 
 
@@ -43,7 +42,7 @@ class GTS:
     def addEdge(self, a, b,c): 
         temp = [a, b,c]
         self.visual.append(temp)
-        self.edge_colors[(a, b)] = "gray" 
+        self.edge_colors[(a, b)] = "gray"  
 
     def draw_graph(self):
 
@@ -69,7 +68,6 @@ class GTS:
             font_color="black",
             edge_color=[self.edge_colors[(a, b)] if (a, b) in self.edge_colors else "gray" for a, b in self.G_main.edges()],
         )
-
         edge_labels = nx.get_edge_attributes(self.G_main, 'weight')
         nx.draw_networkx_edge_labels(self.G_main, pos, edge_labels=edge_labels)
         self.canvas.draw()
@@ -142,106 +140,45 @@ class GTS:
         start = self.GUI.e_start.get()
         start = start.upper()
         check_data=0
-        if start == "":
-            messagebox.showinfo("Fail", "Chưa nhập đỉnh bắt đầu")
-            return
-        
         for data in self.visual:
             if start == data[0] or start == data[1]:
                 check_data =1
         if check_data == 0:
-            messagebox.showinfo("Fail", "Đỉnh không tồn tại")
             return
-        
-        vertices = set()
-        for edge in self.visual:
-            vertices.add(edge[0])
-            vertices.add(edge[1])
-
-        if len(vertices) < 2:
-            messagebox.showinfo("Fail", "Đồ thị phải có hơn 2 đỉnh")
-            return
-        
-        flag = start
+        self.visual_temp = []
+        check_visual = []
+        solve = 0
+        flat = start
+        check = 0
+        min_visual = None
         slove = 0
-        unvisited = vertices - {start}
-
-        while unvisited:
-            min_vertices = None
-            min_distance = float('inf')
+        while True:
+            min_line = 1000000000000000
             for data in self.visual:
-                if data[0] == flag and data[1] in unvisited and float(data[2])<min_distance:
-                    min_distance = float(data[2])
-                    min_vertices = data[1]
-                elif data[1] == flag and data[0] in unvisited and float(data[2])<min_distance:
-                    min_distance = float(data[2])
-                    min_vertices = data[0]
-
-            if min_vertices is None:
-                messagebox.showinfo("Fail", "Đồ thị không liên thông")
-                return
-            
-            data =[flag,min_vertices]
-            self.visual_temp.append(data)
-            slove += min_distance
-            flag = min_vertices
-            unvisited.remove(min_vertices)
-        
-        for data in self.visual:
-            if (data[0] == flag and data[1] == start) or (data[1] == flag and data[0] == start):
-                slove += float(data[2])
-                last_data =[data[0],data[1]]
-                self.visual_temp.append(last_data)
+                if (data[0]== start and data[1] not in check_visual) or (data[1] == start and data[0] not in check_visual):
+                    if int(data[2]) < min_line:
+                        min_line = int(data[2])
+                        if data[0] != start:
+                            min_visual = data[0]
+                        else:
+                            min_visual = data[1]
+                        if min_line != 1000000000000000:
+                            slove += min_line
+                            self.visual_temp.append(data)
+            if start not in check_visual:
+                check_visual.append(start)
+            if check == len(check_visual):
+                check_visual.remove(flat)
+                check = 10000000000
+            start = min_visual
+            if check < len(check_visual):
+                check = len(check_visual)
+            if start == flat:
+                self.draw_GTS()
+                self.GUI.l_solve.config(text = f"Độ dài đường đi là: {slove}")
                 break
 
-        self.draw_GTS()
-        self.GUI.e_solve.configure(state="normal")
-        self.GUI.e_solve.delete(0, 'end')
-        self.GUI.e_solve.insert(0,slove)
-        self.GUI.e_solve.configure(state="disabled")
-
-    def save_file(self):
-        if self.visual == []:
-            messagebox.showerror("Error", f"Chưa có dữ liệu đồ thị")
-            return
-        file_path = filedialog.asksaveasfilename(
-            defaultextension=".txt",
-            filetypes=[("Text files", "*.txt"), ("All files", "*.*")],
-            title="Lưu đồ thị"
-        )
-
-        if file_path:
-            try:
-                with open(file_path, 'w') as file:
-                    for edge in self.visual:
-                        file.write(f"{edge[0]} - {edge[1]} - {edge[2]}\n")
-                messagebox.showinfo("Success", f"Đã lưu đồ thị vào file '{file_path}' thành công!")
-            except Exception as e:
-                messagebox.showerror("Error", f"Lỗi khi lưu file: {str(e)}")
-
-    def open_file(self):
-        file_path = filedialog.askopenfilename(
-        filetypes=[("Text files", "*.txt"), ("All files", "*.*")],
-        title="Mở file đồ thị"
-        )
-
-        if file_path:
-            try:
-                self.visual = [] 
-                self.edge_colors = {}  
-                with open(file_path, 'r') as file:
-                    for edge in file:
-                        parts = edge.strip().split(' - ')
-                        if len(parts) == 3 and self.is_number(parts[2]):
-                            a, b, c = parts[0], parts[1], parts[2]
-                            self.addEdge(a, b, c)
-                        self.draw_graph()
-                        self.show()
-                    messagebox.showinfo("Success", f"tải đồ thị từ file '{file_path}' thành công!")
-            except Exception as e:
-                messagebox.showerror("Error", f"Lỗi khi mở file: {str(e)}")
-
-
+        
     def contruction(self):
         self.GUI.title ("GTS")
         self.GUI.geometry('960x520+300+150')
@@ -278,33 +215,20 @@ class GTS:
         self.GUI.e_khoangcach = tk.Entry(self.GUI.frame_function, width=9, justify='left', font=('Ivy', 11), highlightthickness=1, relief="solid")
         self.GUI.e_khoangcach.place(x=105, y=70)
 
-        self.GUI.b_draw_graph = tk.Button(self.GUI.frame_function, text="Draw grap", width=10, height=1, bg=co4, font=('Ivy 8 bold'),command=self.draw_graph)
+        self.GUI.b_draw_graph = tk.Button(self.GUI.frame_function, text="draw grap", width=10, height=1, bg=co4, font=('Ivy 8 bold'),command=self.add_graph)
         self.GUI.b_draw_graph.place(x=10, y=110)
 
-        self.GUI.b_add_distance = tk.Button(self.GUI.frame_function, text="Add distance", width=10, height=1, bg=co4, font=('Ivy 8 bold'),command=self.add_graph)
-        self.GUI.b_add_distance.place(x=210, y=40)
-
-        self.GUI.b_reset_graph = tk.Button(self.GUI.frame_function, text="Reset", width=10, height=1, bg=co4, font=('Ivy 8 bold'),command=self.reset_graph)
+        self.GUI.b_reset_graph = tk.Button(self.GUI.frame_function, text="reset", width=10, height=1, bg=co4, font=('Ivy 8 bold'),command=self.reset_graph)
         self.GUI.b_reset_graph.place(x=110, y=110)
 
-        self.GUI.b_save_graph = tk.Button(self.GUI.frame_function, text="Save to file", width=10, height=1, bg=co4, font=('Ivy 8 bold'),command=self.save_file)
-        self.GUI.b_save_graph.place(x=210, y=110)
-
-        self.GUI.b_open_graph = tk.Button(self.GUI.frame_function, text="Open file", width=10, height=1, bg=co4, font=('Ivy 8 bold'),command=self.open_file)
-        self.GUI.b_open_graph.place(x=210, y=10)
+        self.GUI.b_reset_graph = tk.Button(self.GUI.frame_function, text="GTS", width=10, height=1, bg=co4, font=('Ivy 8 bold'),command=self.GTS)
+        self.GUI.b_reset_graph.place(x=200, y=110)
 
         self.GUI.e_start = tk.Entry(self.GUI.frame_product, width=9, justify='left', font=('Ivy', 11), highlightthickness=1, relief="solid")
         self.GUI.e_start.place(x=10, y=170)
 
-        self.GUI.b_GTS = tk.Button(self.GUI.frame_product, text="GTS", width=10, height=1, bg=co4, font=('Ivy 8 bold'),command=self.GTS)
-        self.GUI.b_GTS.place(x=100, y=170)
-
-        self.GUI.l_solve = tk.Label(self.GUI.frame_product,text="SLOVE:", font=('Ivy', 11), bg=co0,fg=co1)
+        self.GUI.l_solve = tk.Label(self.GUI.frame_product, font=('Ivy', 11), bg=co0,fg=co1)
         self.GUI.l_solve.place(x=10, y=200)
-
-        self.GUI.e_solve = tk.Entry(self.GUI.frame_product, width=9, justify='left', font=('Ivy', 11), highlightthickness=1, relief="solid")
-        self.GUI.e_solve.place(x=70, y=200)
-        self.GUI.e_solve.configure(state="disabled")
 
         app_name = tk.Label(self.GUI.frame_up, text="GTS", height=1, font=('Verdana 17 bold'), bg=co4 ,fg=co1)
         app_name.place(x=5, y=5) 
